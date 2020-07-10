d043bca54533ad5713ff8be83073e33acee2c573
Melody <502803662@qq.com>
2020-07-10 22:14:20 +0800

    this is live environment

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <video id="my-video" class="video-js vjs-default-skin" controls preload="auto" >
+    <!-- 直播地址就是nginx映射后的播放地址，注意后缀为直播流的m3u8 -->
+      <source src="http://192.168.99.100:8080/stream/test.m3u8" >
+    </video>
   </div>
 </template>
+
+<script>
+import videojs from 'video.js';
+import 'videojs-contrib-hls';
+
+export default {
+  mounted() {
+    videojs('my-video', {
+      bigPlayButton: true,
+      textTrackDisplay: false,
+      posterImage: true,
+      errorDisplay: false,
+      controlBar: true,
+    }, function () {
+      this.play();
+    });
+  },
+};
+</script>

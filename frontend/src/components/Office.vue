7b4f281f29b1d58f25f6477cf732f8a52d4c75cf
张弼铖 <zzz>
2020-07-14 17:32:18 +0800

    add some pages for test Ref #19

---
+++ frontend/src/components/Office.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="office">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'office.vue',
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+
+</style>

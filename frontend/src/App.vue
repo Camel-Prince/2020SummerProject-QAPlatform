4062db0e9027810fc8ecdba8d5a504bdb0b7d703
王子旭 <1784831621@qq.com>
2020-07-14 17:36:17 +0800

    reconstruct hompage-style Ref #8

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
-<!--    <div id="nav">-->
-<!--      <router-link to="/">Home</router-link> |-->
-<!--      <router-link to="/about">About</router-link>-->
-<!--    </div>-->
-    <code-editor v-model="content"></code-editor>
-<!--    <router-view/>-->
+   <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+};
+</script>
+
 <style lang="scss">
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -31,17 +35,3 @@
   }
 }
 </style>
-<script>
-import CodeEditor from './components/CodeEditor.vue';
-
-export default {
-  components: {
-    CodeEditor,
-  },
-  data() {
-    return {
-      content: '',
-    };
-  },
-};
-</script>
---
+++ frontend/src/views/TeacherHomepage.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+       <el-container class="homepageHeaderContainer">
+           <h1 class="mainTitle">
+               计发客Q&A直播平台
+           </h1>
+       </el-container>
+       <el-container>
+           <el-aside class="aside">
+               aside
+           </el-aside>
+           <el-main class="main">
+               main
+           </el-main>
+       </el-container>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TeacherHomepage',
+};
+</script>
+
+<style scoped>
+body {
+    margin: 0;
+}
+
+.homepageHeaderContainer {
+    background-color: rgb(64,158,255);
+    height: 10vh;
+}
+
+.mainTitle {
+    width: 250px;
+    margin-left: 30px;
+    margin-right: 1330px;
+    color: white;
+    font-family:
+     "Helvetica Neue",
+     Helvetica,
+     "PingFang SC",
+     "Hiragino Sans GB",
+     "Microsoft YaHei",
+     "微软雅黑",
+     Arial,
+     sans-serif;
+    float: left;
+}
+
+.aside {
+    background-color: grey;
+    height: 90vh;
+    }
+
+.main {
+    background-color: white;
+    height: 90vh;
+}
+</style>

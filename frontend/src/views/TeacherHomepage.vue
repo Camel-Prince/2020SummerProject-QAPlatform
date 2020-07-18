eb1b0f4612205ffe7982ff722d87963f0d1e28c7
王子旭 <1784831621@qq.com>
2020-07-18 11:26:18 +0800

    Update TeacherHomepage.vue

--- frontend/src/views/TeacherHomepage.vue
+++ frontend/src/views/TeacherHomepage.vue
@@ -1,119 +1,119 @@
 <template>
-    <div>
-        <homepage-header></homepage-header>
-       <el-container>
-           <teacher-homepage-aside activeItemFromViews="1"></teacher-homepage-aside>
-           <el-main class="main">
-               <span class="table-title">教师{{teacherName}}好，您的直播课程表见下</span>
-               <el-table class="room-table" :data="roomData" size="medium" border="true">
-                   <el-table-column type="expand">
-                       <template slot-scope="scope">
-                           <el-table class="time-list-table" :data="scope.row.useTimeList"
-                           highlight-current-row border="true">
-                               <el-table-column label="Index" type="index" width="100">
-                               </el-table-column>
-                               <el-table-column label="开始时间" width="180">
-                                   <template slot-scope="innerScope">
-                                       <p>{{innerScope.row.startTime}}</p>
-                                   </template>
-                               </el-table-column>
-                               <el-table-column label="结束时间" width="180">
-                                   <template slot-scope="innerScope">
-                                       <p>{{innerScope.row.endTime}}</p>
-                                   </template>
-                               </el-table-column>
-                               <el-table-column label="编辑直播间信息" width="180">
-                                   <template slot-scope="innerScope">
-                                     <el-button
-                                    @click="deleteLiveTime(scope.row.pk, innerScope.row.timePk)"
-                                    type="danger" round>
-                                       删除预定
-                                     </el-button>
-                                   </template>
-                               </el-table-column>
-                           </el-table>
-                           <el-button @click="addDialogFormVisible = true" type="primary" plain>
-                              添加
-                            </el-button>
-                            <el-dialog :title="'请为课程'+scope.row.name+'添加直播时间'"
-                            :visible.sync="addDialogFormVisible">
-                                <el-form :model="form">
-                                    <el-form-item label="日期" :label-width="formLabelWidth">
-                                        <el-date-picker
-                                        v-model="form.date"
-                                        align="right"
-                                        type="date"
-                                        placeholder="选择日期"
-                                        :picker-options="dateQuickPickerOptions">
-                                        </el-date-picker>
-                                    </el-form-item>
-                                    <el-form-item label="开始时间" :label-width="formLabelWidth">
-                                      <el-time-select
-                                        placeholder="开始时间"
-                                        v-model="form.startTime"
-                                        :picker-options="{
-                                        start: '08:30',
-                                        step: '00:15',
-                                        end: '18:30'
-                                        }">
-                                      </el-time-select>
-                                    </el-form-item>
-                                    <el-form-item label="结束时间" :label-width="formLabelWidth">
-                                      <el-time-select
-                                        placeholder="结束时间"
-                                        v-model="form.endTime"
-                                        :picker-options="{
-                                        start: '08:30',
-                                        step: '00:15',
-                                        end: '18:30',
-                                        minTime: form.startTime
-                                        }">
-                                      </el-time-select>
-                                    </el-form-item>
-                                </el-form>
-                                <div slot="footer" class="dialog-footer">
-                                        <el-button @click="addDialogFormVisible = false">
-                                            取 消
-                                        </el-button>
-                                        <el-button type="primary"
-                                        @click="addLiveTime(scope.row.pk)">
-                                            确 定
-                                        </el-button>
-                                </div>
-                            </el-dialog>
-                       </template>
-                   </el-table-column>
-                   <el-table-column label="课程名称"  width="180">
-                        <template slot-scope="scope">
-                           <el-popover trigger="hover" placement="top">
-                               <p>课程id：{{scope.row.courseID}}</p>
-                               <p>课程描述：{{scope.row.desc}}</p>
-                               <div slot="reference" class="name-wrapper">
-                                 <h1>{{ scope.row.name }}</h1>
-                               </div>
-                           </el-popover>
-                       </template>
-                   </el-table-column>
-                   <el-table-column label="图像" width="200">
-                       <template slot-scope="scope">
-                           <el-image class="courseImg" :src="scope.row.img"></el-image>
-                       </template>
-                   </el-table-column>
-                   <el-table-column label="课程信息" width="280">
-                       <template slot-scope="scope">
-                           <p>课程序号：{{scope.row.courseID}}</p>
-                           <p>课程描述：{{scope.row.desc}}</p>
-                       </template>
-                   </el-table-column>
-                   <el-table-column label="进入直播间" width="180">
-                       <el-button @click="enterLiveRoom" type="success" plain>
-                           进入
-                       </el-button>
-                   </el-table-column>
-               </el-table>
-           </el-main>
-       </el-container>
-    </div>
+  <div>
+    <homepage-header></homepage-header>
+    <el-container>
+      <teacher-homepage-aside activeItemFromViews="1"></teacher-homepage-aside>
+      <el-main class="main">
+        <span class="table-title">教师{{teacherName}}好，您的直播课程表见下</span>
+        <el-table class="room-table" :data="roomData" size="medium" border="true">
+          <el-table-column type="expand">
+            <template slot-scope="scope">
+              <el-table class="time-list-table" :data="scope.row.useTimeList"
+              highlight-current-row border="true">
+                <el-table-column label="Index" type="index" width="100">
+                </el-table-column>
+                <el-table-column label="开始时间" width="180">
+                  <template slot-scope="innerScope">
+                    <p>{{innerScope.row.startTime}}</p>
+                  </template>
+                </el-table-column>
+                <el-table-column label="结束时间" width="180">
+                  <template slot-scope="innerScope">
+                    <p>{{innerScope.row.endTime}}</p>
+                  </template>
+                </el-table-column>
+                <el-table-column label="编辑直播间信息" width="180">
+                  <template slot-scope="innerScope">
+                    <el-button
+                    @click="deleteLiveTime(scope.row.pk, innerScope.row.timePk)"
+                    type="danger" round>
+                      删除预定
+                    </el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+              <el-button @click="addDialogFormVisible = true" type="primary" plain>
+                添加
+              </el-button>
+              <el-dialog :title="'请为课程'+scope.row.name+'添加直播时间'"
+              :visible.sync="addDialogFormVisible">
+                <el-form :model="form">
+                  <el-form-item label="日期" :label-width="formLabelWidth">
+                    <el-date-picker
+                    v-model="form.date"
+                    align="right"
+                    type="date"
+                    placeholder="选择日期"
+                    :picker-options="dateQuickPickerOptions">
+                    </el-date-picker>
+                  </el-form-item>
+                  <el-form-item label="开始时间" :label-width="formLabelWidth">
+                    <el-time-select
+                    placeholder="开始时间"
+                    v-model="form.startTime"
+                    :picker-options="{
+                    start: '08:30',
+                    step: '00:15',
+                    end: '18:30'
+                    }">
+                    </el-time-select>
+                  </el-form-item>
+                  <el-form-item label="结束时间" :label-width="formLabelWidth">
+                    <el-time-select
+                    placeholder="结束时间"
+                    v-model="form.endTime"
+                    :picker-options="{
+                    start: '08:30',
+                    step: '00:15',
+                    end: '18:30',
+                    minTime: form.startTime
+                    }">
+                    </el-time-select>
+                  </el-form-item>
+                </el-form>
+                <div slot="footer" class="dialog-footer">
+                  <el-button @click="addDialogFormVisible = false">
+                    取 消
+                  </el-button>
+                  <el-button type="primary"
+                  @click="addLiveTime(scope.row.pk)">
+                    确 定
+                  </el-button>
+                </div>
+              </el-dialog>
+            </template>
+          </el-table-column>
+          <el-table-column label="课程名称"  width="180">
+            <template slot-scope="scope">
+              <el-popover trigger="hover" placement="top">
+                <p>课程id：{{scope.row.courseID}}</p>
+                <p>课程描述：{{scope.row.desc}}</p>
+                <div slot="reference" class="name-wrapper">
+                  <h1>{{ scope.row.name }}</h1>
+                </div>
+              </el-popover>
+            </template>
+          </el-table-column>
+          <el-table-column label="图像" width="200">
+            <template slot-scope="scope">
+              <el-image class="courseImg" :src="scope.row.img"></el-image>
+            </template>
+          </el-table-column>
+          <el-table-column label="课程信息" width="280">
+            <template slot-scope="scope">
+              <p>课程序号：{{scope.row.courseID}}</p>
+              <p>课程描述：{{scope.row.desc}}</p>
+            </template>
+          </el-table-column>
+          <el-table-column label="进入直播间" width="180">
+            <el-button @click="enterLiveRoom" type="success" plain>
+              进入
+            </el-button>
+          </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script>
@@ -289,44 +289,44 @@ export default {
 
 <style scoped>
 body {
-    margin: 0;
+  margin: 0;
 }
 
 .main {
-    background-color: white;
-    height: 90vh;
+  background-color: white;
+  height: 90vh;
 }
 
 .course-img {
-    width: 150px;
-    height: 100px;
+  width: 150px;
+  height: 100px;
 }
 .room-table {
-    width: 848px;
-    position: relative;
-    left: 20%;
-    top: 40px;
+  width: 848px;
+  position: relative;
+  left: 20%;
+  top: 40px;
 }
 .time-list-table {
-    width: 640px;
+  width: 640px;
 }
 .table-title {
-    width: 850px;
-    margin-left: 30px;
-    margin-right: 1330px;
-    color: black;
-    font-family:
-     "Helvetica Neue",
-     Helvetica,
-     "PingFang SC",
-     "Hiragino Sans GB",
-     "Microsoft YaHei",
-     "微软雅黑",
-     Arial,
-     sans-serif;
-    font-size: 40px;
-    float: left;
-    margin-left: 0;
-    text-align: left;
+  width: 850px;
+  margin-left: 30px;
+  margin-right: 1330px;
+  color: black;
+  font-family:
+    "Helvetica Neue",
+    Helvetica,
+    "PingFang SC",
+    "Hiragino Sans GB",
+    "Microsoft YaHei",
+    "微软雅黑",
+    Arial,
+    sans-serif;
+  font-size: 40px;
+  float: left;
+  margin-left: 0;
+  text-align: left;
 }
 </style>

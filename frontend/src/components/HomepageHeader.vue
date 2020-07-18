70ac990d51be6cb75b6ebdd129963b3914f93fac
尤旺 <502803662@qq.com>
2020-07-18 10:20:44 +0800

    Update HomepageHeader.vue

--- frontend/src/components/HomepageHeader.vue
+++ frontend/src/components/HomepageHeader.vue
@@ -1,9 +1,9 @@
 <template>
-     <el-container class="homepageHeaderContainer">
-           <h1 class="mainTitle">
-               计发客Q&A直播平台
-           </h1>
-       </el-container>
+  <el-container class="homepageHeaderContainer">
+    <h1 class="mainTitle">
+      计发客Q&A直播平台
+    </h1>
+  </el-container>
 </template>
 
 <script>
@@ -14,24 +14,24 @@ export default {
 
 <style scoped>
 .homepageHeaderContainer {
-    background-color: rgb(64,158,255);
-    height: 10vh;
+  background-color: rgb(64,158,255);
+  height: 10vh;
 }
 
 .mainTitle {
-    width: 250px;
-    margin-left: 30px;
-    margin-right: 1330px;
-    color: white;
-    font-family:
-     "Helvetica Neue",
-     Helvetica,
-     "PingFang SC",
-     "Hiragino Sans GB",
-     "Microsoft YaHei",
-     "微软雅黑",
-     Arial,
-     sans-serif;
-    float: left;
+  width: 250px;
+  margin-left: 30px;
+  margin-right: 1330px;
+  color: white;
+  font-family:
+  "Helvetica Neue",
+  Helvetica,
+  "PingFang SC",
+  "Hiragino Sans GB",
+  "Microsoft YaHei",
+  "微软雅黑",
+  Arial,
+  sans-serif;
+  float: left;
 }
 </style>

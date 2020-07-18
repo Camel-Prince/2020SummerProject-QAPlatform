706c7f892c76c0d67796a6b3f5172172a619b734
王子旭 <1784831621@qq.com>
2020-07-18 12:08:54 +0800

    Update TeacherRecentlive.vue

--- frontend/src/views/TeacherRecentlive.vue
+++ frontend/src/views/TeacherRecentlive.vue
@@ -1,43 +1,43 @@
 <template>
-    <div>
-        <homepage-header></homepage-header>
-       <el-container>
-           <teacher-homepage-aside></teacher-homepage-aside>
-           <el-main class="main">
-               <el-table :data="room_data">
-                   <el-table-column label="课程名称" width="180">
-                       <template slot-scope="scope">
-                           <el-popover trigger="hover" placement="top">
-                               <p>课程id：{{scope.row.course_id}}</p>
-                               <p>课程描述：{{scope.row.desc}}</p>
-                               <div slot="reference" class="name-wrapper">
-                                 <el-tag size="medium">{{ scope.row.name }}</el-tag>
-                               </div>
-                           </el-popover>
-                       </template>
-                   </el-table-column>
-                   <el-table-column label="开课时间表" width="280">
-                       <template slot-scope="outterScope">
-                           <el-table :data="outterScope.row.use_time_list">
-                             <el-table-column label="详细时间" width="280">
-                                 <template slot-scope="innerScope">
-                                     <p>日期：{{innerScope.row.date}}</p>
-                                     <p>开始：{{innerScope.row.start_time}}</p>
-                                     <p>结束：{{innerScope.row.end_time}}</p>
-                                 </template>
-                             </el-table-column>
-                             <el-table-column label="选择">
-                               <el-button>
-                                   进入直播间
-                               </el-button>
-                             </el-table-column>
-                           </el-table>
-                       </template>
-                   </el-table-column>
-               </el-table>
-           </el-main>
-       </el-container>
-    </div>
+  <div>
+    <homepage-header></homepage-header>
+    <el-container>
+      <teacher-homepage-aside></teacher-homepage-aside>
+      <el-main class="main">
+        <el-table :data="room_data">
+          <el-table-column label="课程名称" width="180">
+            <template slot-scope="scope">
+              <el-popover trigger="hover" placement="top">
+                <p>课程id：{{scope.row.course_id}}</p>
+                <p>课程描述：{{scope.row.desc}}</p>
+                <div slot="reference" class="name-wrapper">
+                  <el-tag size="medium">{{ scope.row.name }}</el-tag>
+                </div>
+              </el-popover>
+            </template>
+          </el-table-column>
+          <el-table-column label="开课时间表" width="280">
+            <template slot-scope="outterScope">
+              <el-table :data="outterScope.row.use_time_list">
+                <el-table-column label="详细时间" width="280">
+                  <template slot-scope="innerScope">
+                    <p>日期：{{innerScope.row.date}}</p>
+                    <p>开始：{{innerScope.row.start_time}}</p>
+                    <p>结束：{{innerScope.row.end_time}}</p>
+                  </template>
+                </el-table-column>
+                <el-table-column label="选择">
+                  <el-button>
+                     进入直播间
+                  </el-button>
+                </el-table-column>
+              </el-table>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script>
@@ -100,11 +100,11 @@ export default {
 
 <style scoped>
 body {
-    margin: 0;
+  margin: 0;
 }
 
 .main {
-    background-color: white;
-    height: 90vh;
+  background-color: white;
+  height: 90vh;
 }
 </style>

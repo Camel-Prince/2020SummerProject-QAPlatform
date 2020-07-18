3539e6a74787cb6fbbc90b31a42b9c2d8259bee6
尤旺 <502803662@qq.com>
2020-07-18 10:28:10 +0800

    Update TeacherHomepageAside.vue

--- frontend/src/components/TeacherHomepageAside.vue
+++ frontend/src/components/TeacherHomepageAside.vue
@@ -1,30 +1,30 @@
 <template>
-    <el-aside class="aside">
-          <el-menu :default-active="activeItemFromViews" active-text-color="#409EFF">
-            <el-menu-item index="1" @click="toHomePage">
-                  <i class="el-icon-s-home"></i>
-                 <span>
-                     教师首页
-                 </span>
-              </el-menu-item>
-              <el-menu-item index="2" @click="toInformationPage">
-                  <i class="el-icon-user"></i>
-                 <span>
-                     个人信息
-                 </span>
-              </el-menu-item>
-              <el-menu-item index="3" @click="toRecentLive">
-                  <i class="el-icon-video-camera"></i>
-                  <span>近期直播</span>
-              </el-menu-item>
-              <el-menu-item index="4" @click="toLiveTime">
-                  <i class="el-icon-notebook-2"></i>
-                  <span>
-                    直播时长
-                  </span>
-              </el-menu-item>
-          </el-menu>
-      </el-aside>
+  <el-aside class="aside">
+    <el-menu :default-active="activeItemFromViews" active-text-color="#409EFF">
+      <el-menu-item index="1" @click="toHomePage">
+        <i class="el-icon-s-home"></i>
+          <span>
+            教师首页
+          </span>
+      </el-menu-item>
+      <el-menu-item index="2" @click="toInformationPage">
+        <i class="el-icon-user"></i>
+          <span>
+           个人信息
+          </span>
+      </el-menu-item>
+      <el-menu-item index="3" @click="toRecentLive">
+        <i class="el-icon-video-camera"></i>
+        <span>近期直播</span>
+      </el-menu-item>
+      <el-menu-item index="4" @click="toLiveTime">
+        <i class="el-icon-notebook-2"></i>
+          <span>
+            直播时长
+          </span>
+      </el-menu-item>
+    </el-menu>
+  </el-aside>
 </template>
 
 <script>
@@ -58,7 +58,7 @@ export default {
 
 <style lang="less" scoped>
 .aside {
-    background-color: white;
-    height: 90vh;
+  background-color: white;
+  height: 90vh;
 }
 </style>

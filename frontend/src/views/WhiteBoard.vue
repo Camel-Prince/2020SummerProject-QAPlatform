08c9eda1477028962e75b5cdcc075e3bf9808851
尤旺 <502803662@qq.com>
2020-07-21 00:32:35 +0800

    Update Whiteboard.vue

---
+++ frontend/src/views/WhiteBoard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Whiteboard">
+    <whiteborb/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Whiteborb from '@/components/Whiteborb.vue';
+
+export default {
+  name: 'Whiteboard',
+  components: {
+    whiteborb: Whiteborb,
+  },
+};
+</script>

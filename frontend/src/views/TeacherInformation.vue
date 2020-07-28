4eb55dddafecdde4811eb7d39ca27ce1b78ab5e8
王子旭 <1784831621@qq.com>
2020-07-28 23:37:00 +0800

    Delete some useless files and add a little functions in TeacherInformation.vue. Ref #108

--- frontend/src/views/TeacherInformation.vue
+++ frontend/src/views/TeacherInformation.vue
@@ -4,13 +4,27 @@
     <el-container>
       <teacher-homepage-aside activeItemFromViews="2"></teacher-homepage-aside>
       <el-main class="main">
-        teacher-Information-Table;
+        <el-form>
+          <el-form-item label="教师姓名">
+             <p>{{teacherName}}</p>
+          </el-form-item>
+          <el-form-item label="教师ID">
+             <p>{{teacherID}}</p>
+          </el-form-item>
+          <el-form-item label="教师头像">
+             <img src="teacherImg"/>
+          </el-form-item>
+          <el-form-item label="教师用昵称（用户名）">
+             <p>{{teacherUserName}}</p>
+          </el-form-item>
+        </el-form>
       </el-main>
     </el-container>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import homepageHeader from '../components/HomepageHeader.vue';
 import teacherHomepageAside from '../components/TeacherHomepageAside.vue';
 
@@ -19,6 +33,28 @@ export default {
   components: {
     HomepageHeader: homepageHeader,
     TeacherHomepageAside: teacherHomepageAside,
+  },
+  data() {
+    return {
+      teacherName: '',
+      teacherID: '',
+      teacherImg: '',
+      teacherUserName: '',
+    };
+  },
+  mounted() {
+    axios({
+      method: 'get',
+      url: 'http://localhost:8000/QAplatform/detail/',
+      headers: {
+        Authorization: `jwt ${window.sessionStorage.getItem('token')}`,
+      },
+    })
+      .then((response) => {
+        this.teacherName = response.data.data.name;
+        this.teacherID = response.data.data.id_card;
+        this.teacherUserName = response.data.data.user_info.username;
+      });
   },
 };
 </script>
